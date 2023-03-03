636e2af2327d47b9a4b1642f4006a510a667937f
Vazha Omanashvili <seeded-enterprise-owner@example.com>
2023-03-03 12:49:04 +0200

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjIy/spectral.js?branch_slug=main&token=e6d1b4d6-4009-45ad-a9c8-7e3f748be5da";
+export default { extends: ruleset };
